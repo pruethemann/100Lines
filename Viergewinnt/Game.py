8539cf7573d8cd59170cc92205a4669afd1e6211
pixelline <peter.ruethemann@gmail.com>
2020-07-31 22:33:15 +0200

    Implemented simple agents

--- Viergewinnt/Game.py
+++ Viergewinnt/Game.py
@@ -1,11 +1,16 @@
 import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
-
-
+import random
+import matplotlib.pyplot as plt
+import seaborn  as sns
+import sys
 
 class Viergewinnt:
-    def __init__(self, nrows=6, ncols=8):
+    def __init__(self):
+        pass
+
+    def start_game(self,nrows=6, ncols=9):
         """
         Creates a new game instance
         """
@@ -16,88 +21,243 @@ class Viergewinnt:
         self.ncols = ncols
 
         self.board = np.zeros((nrows,ncols), dtype=np.uint8)
-        """
-        print("Start Windows")
-        window = tk.Tk()
-        greeting = tk.Label(text="Vier gewinnt")
-        greeting.pack()
-        print("Start Windows")
-        window.mainloop()
-        """
+        self.player = 1
+        self.last_col = 0
+        self.move_count = 0
 
-    def gameplay(self):
-        if self.check_win():
-            print(f'Player {self.player} won')
+    def start_testgame(self,nrows=6, ncols=8):
+        """
+        Creates a new game instance
+        """
+        self.nrows = nrows
+        self.ncols = ncols
 
-        if self.player == 1:
-            self.player = 2
+        self.board = np.zeros((nrows,ncols), dtype=np.uint8)
+        self.player = 1
+        self.board[0,2]=2
+        self.board[1, 3] = 2
+        self.board[2, 4] = 2
+        self.board[3, 5] = 1
+        self.print_board()
+        self.check_win()
+
+
+    def gameplay(self, strategy) -> int:
+        # load player strategy
+
+        while True:
+            # move of player
+            self.move(strategy=strategy[self.player-1])
+
+            # Check win
+            if self.check_win():
+                return self.player
+
+            if self.move_count == self.ncols * self.nrows:
+                print('Unentschieden')
+                return -1
+
+            # change player
+            if self.player == 1:
+                self.player = 2
+            else:
+                self.player = 1
+
+        #print(f'Player {self.player} won')
+        #self.print_board()
+
+
+    def competition(self, n=1000, strategy=None):
+
+        win_1 = 0
+        pat = 0
+        for i in range(n):
+            self.start_game()
+            win = self.gameplay(strategy)
+            if win == 1:
+                win_1 += 1
+
+            elif win == -1:
+                pat += 1
+
+            if n<=10:
+                self.print_board()
+
+        # Yield
+        player_1 = win_1/n
+        player_2 = (n - win_1 - pat) / n
+
+        print(f'Strategy {strategy[0]} : {player_1} {win_1}')
+        print(f'Strategy {strategy[1]} : {player_2} {n - win_1 - pat}')
+        print(f'Strategy {strategy[1]} : {pat / n} {pat}')
+
+    def move(self,strategy='random'):
+        if strategy == 'random':
+            self.str_random()
+        elif strategy == 'left':
+            self.str_left()
+
+        elif strategy == 'manual':
+            self.str_manual()
+
+        elif strategy == 'mimic':
+            self.str_mimic()
+
+        elif strategy == 'mimic_next':
+            self.str_mimic_next()
+
+        elif strategy == 'middle':
+            self.str_middle()
+
         else:
-            self.player = 1
+            self.str_random()
 
+    def str_random(self):
+        """
+        Random strategy
+        """
+
+        # get all columns with at least one empty slot
+        cols = [c for c in range(self.ncols) if self.check_drop(c)]
+
+        x = random.choice(cols)
+        self.drop(x, self.player)
+
+    def str_left(self):
+        """
+        Place as left as possible
+        """
+        # get all columns with at least one empty slot
+        cols = [c for c in range(self.ncols) if self.check_drop(c)]
+
+        x = cols[0]
+        self.drop(x, self.player)
+
+    def str_middle(self):
+        """
+        Place in middle
+        """
+        # get all columns with at least one empty slot
+        if not self.drop(self.ncols // 2, self.player):
+            self.str_random()
+
+    def check_drop(self,col):
+        for row in range(self.nrows-1,-1,-1):
+            if self.board[row,col] == 0:
+                return True
+        return False
 
     def print_board(self):
         print(self.board)
 
     def drop(self, col, player):
+        if col < 0 or col >= self.ncols:
+            return False
+
         for row in range(self.nrows-1,-1,-1):
             if self.board[row,col] == 0:
                 self.board[row, col] = player
-                return
+                self.last_row = row
+                self.last_col = col
+                self.move_count += 1
+                return True
 
-        print(f'Col {col} is full')
+        #print(f'Col {col} is full')
+        return False
 
     def check_win(self):
-        # horicontal
-        streak = 0
+        # vertical
         for col in range(self.ncols):
+            streak_1 = 0
+            streak_2 = 0
             for row in range(self.nrows):
-                if self.board[row,col] in [1,2]:
-                    streak += 1
-                else:
-                    streak = 0
+                if self.board[row, col] == 1:
+                    streak_1 += 1
+                    streak_2 = 0
 
-                if streak == 4:
+                elif self.board[row, col] == 2:
+                    streak_2 += 1
+                    streak_1 = 0
+
+                else:
+                    streak_1 = 0
+                    streak_2 = 0
+
+               # if streak_1 > 3 or streak_2 > 3:
+                #    print('vertical')
+                 #   print(streak_1, streak_2, row, col)
+
+                if streak_1 == 4 or streak_2 == 4:
+                    #print("vertcial", row, col)
                     return True
 
-        # vertical
-        streak = 0
+        # horicontal
+
         for row in range(self.nrows):
+            streak_1 = 0
+            streak_2 = 0
             for col in range(self.ncols):
-                if self.board[row, col] in [1, 2]:
-                    streak += 1
+                if self.board[row, col] == 1:
+                    streak_1 += 1
+                    streak_2 = 0
+                elif self.board[row, col] == 2:
+                    streak_2 += 1
+                    streak_1 = 0
                 else:
-                    streak = 0
-                if streak == 4:
+                    streak_1 = 0
+                    streak_2 = 0
+
+                if streak_1 == 4 or streak_2 == 4:
+                    #print("horicontal", row, col)
+                    return True
+
+        # diagonal right
+
+        for col in range(self.ncols - 3):
+            for row in range(self.nrows - 3):
+                if self.board[row,col] == 1 and self.board[row+1,col+1] == 1 and self.board[row+2,col+2] == 1 and self.board[row+3,col+3] == 1:
+                    #print("vertical right", row, col)
+                    return True
+                if self.board[row,col] == 2 and self.board[row+1,col+1] == 2 and self.board[row+2,col+2] == 2 and self.board[row+3,col+3] == 2:
+                    return True
+
+        # diagonal left
+
+        for col in range(3,self.ncols):
+            for row in range(self.nrows - 3):
+                if self.board[row,col] == 1 and self.board[row+1,col-1] == 1 and self.board[row+2,col-2] == 1 and self.board[row+3,col-3] == 1:
+                    #print("vertical left", row, col)
+                    return True
+                if self.board[row,col] == 2 and self.board[row+1,col-1] == 2 and self.board[row+2,col-2] == 2 and self.board[row+3,col-3] == 2:
                     return True
 
         return False
 
-    def testing(self):
-        self.drop(1, 2)
-        self.print_board()
-        self.check_win()
-        self.drop(1, 2)
-        self.print_board()
-        self.check_win()
-        self.drop(1, 2)
-        self.print_board()
-        self.check_win()
-        self.drop(1, 2)
-        self.print_board()
-        self.check_win()
+    def str_mimic(self):
+        if not self.drop(self.last_col, self.player):
+            self.str_random()
 
-        self.drop(0, 1)
+    def str_mimic_next(self):
+        if not self.drop(self.last_col + 1, self.player):
+            self.str_random()
+
+    def str_manual(self):
         self.print_board()
-        self.check_win()
-        self.drop(1, 1)
+        x = input('Welche column? ')
+        self.drop(int(x)-1, self.player)
         self.print_board()
+
+    def testing(self):
+        self.drop(0, 2)
+        print(self.check_win())
+        self.drop(1, 2)
+        print(self.check_win())
+        self.drop(2, 2)
+        print(self.check_win())
+        self.drop(7, 2)
+        print(self.check_win())
         self.check_win()
-        self.drop(2, 1)
         self.print_board()
-        self.check_win()
-        self.drop(3, 1)
-        self.print_board()
-        self.check_win()
 
     def paint_board(self):
         self.print_board()
--- Viergewinnt/Main.py
+++ Viergewinnt/Main.py
@@ -4,6 +4,9 @@ Yeah - I am programming again object oriented
 from Game import Viergewinnt
 
 g = Viergewinnt()
-g.testing()
-g.print_board()
+#g.testing()
+#g.print_board()
 
+game_strategies = ['middle', 'random']
+g.competition(10, game_strategies)
+#g.start_testgame()
